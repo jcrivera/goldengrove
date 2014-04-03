bb8d8cd96a8a9424c6cb67b61eb0e828c5f855f2
ichthala <amottola@constantcontact.com>
2014-04-02 20:44:10 -0400

    Working on controller specs

--- spec/controllers/poems_controller_spec.rb
+++ spec/controllers/poems_controller_spec.rb
@@ -4,36 +4,47 @@ describe PoemsController do
 
   describe "GET #index" do
     it "populates an array of poems" do
-      poem = FactoryGirl.create(:poem)
+      poem = create(:poem)
       get :index
-      assigns(:poems).should eq([poem])
+      expect(assigns(:poems)).to match_array([poem])
     end
+
+    it "returns the 30 most recent poems"
 
     it "renders the index (Salon) view" do
       get :index
-      response.should render_template :index
+      expect(response).to render_template :index
     end
   end
 
   describe "GET #show" do
     it "returns the poem with the given ID" do
-      poem = FactoryGirl.create(:poem)
+      poem = create(:poem)
       get :show, id: poem
-      assigns(:poem).should eq(poem)
+      expect(assigns(:poem)).to eq(poem)
     end
 
     it "renders the show view" do
-      get :show, id: FactoryGirl.create(:contact)
-      response.should render_template :show
+      poem = create(:poem)
+      get :show, id: poem
+      expect(response).to render_template :show
     end
   end
 
   describe "GET #new" do
+    it "assigns a new poem to @poem" do
+      get :new
+      expect(assigns(:poem)).to be_a_new(Poem)
+    end
+
     it "renders the new poem view"
+      get :new
+      expect(:response).to render_template :new
+    end
   end
 
   describe "GET #select_user" do
-    it "populates an array of 9 random users + horse_ebooks"
+    it "populates an array of 10 random users"
   end
 
   describe "POST #write_poem" do
@@ -61,16 +72,6 @@ describe PoemsController do
   describe "DELETE #destroy" do
     it "removes the poem from the database"
     it "redirects to the user's profile page"
-  end
-
-
-
-  describe "title checks" do
-    describe "title_first_post" do
-    end
-
-    describe "title_tennyson" do
-    end
   end
 
 end
